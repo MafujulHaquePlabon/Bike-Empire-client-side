631ef5fbec9cf5b0d83afef922286eaaf0da86a7
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-05-14 08:55:08 +0600

    Inventory Item delivered section done

--- src/components/Pages/ManageItems/ManageItems.js
+++ src/components/Pages/ManageItems/ManageItems.js
@@ -7,7 +7,6 @@ const ManageItems = () => {
     const {id} = useParams();
     const [mItems,setMItems]=useState({});
     const [reload,SetIsReload]=useState(true)
-   // console.log(mItems)
     useEffect(()=>{
         fetch(` https://aqueous-tundra-23230.herokuapp.com/inventoryItems/${id}`)
         .then(res=>res.json())
@@ -34,6 +33,26 @@ const ManageItems = () => {
             event.target.reset();
         })
     }
+    const handleDeliveredInventoryItem=()=>{
+      const quantity= parseInt(mItems.quantity)-1;
+      const UpdatedInventoryItems = {quantity};
+      // send data to the server
+      const url = `https://aqueous-tundra-23230.herokuapp.com/inventoryItems/${id}`;
+      fetch(url, {
+          method: 'PUT',
+          headers: {
+              'content-type': 'application/json'
+          },
+          body: JSON.stringify(UpdatedInventoryItems)
+      })
+      .then(res => res.json())
+      .then(data =>{
+          SetIsReload(!reload)
+          console.log('success', data);
+          alert('InventoryItem Quantity Delivered successfully!!!');
+         
+      })
+    }
    return (
       <div>
           <h2 className="mt-5 text-success ">Manage Items :  </h2>
@@ -51,7 +70,7 @@ const ManageItems = () => {
           <h5>Supplier_name: {mItems.supplier_name}</h5>
          </div>
          <div>
-         <Button /* onClick={()=>navigateToInventoryItemDetail(_id)} */  className="bg-success text-white fw-bolder rounded px-4 py-2 border-0 mb-3 " >Delivered</Button>
+         <Button  onClick={handleDeliveredInventoryItem}   className="bg-success text-white fw-bolder rounded px-4 py-2 border-0 mb-3 " >Delivered</Button>
    <form   onSubmit={handleUpdateInventoryItem}  >
    <input type="quantity" name="quantity" placeholder='quantity' required />
    <input   className="bg-success text-white mb-4 fw-bolder rounded px-4 py-1 border-0"  type="submit" value="Restok" />
